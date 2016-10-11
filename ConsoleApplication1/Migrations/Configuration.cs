1443dabd9216799dfde74dbb3dbae3e555d3df05
Francesco de Guytenaere <guytenaere.fe@gmail.com>
2016-10-11 08:01:05 +0200

    Removed unnecessary usings

--- ConsoleApplication1/Migrations/Configuration.cs
+++ ConsoleApplication1/Migrations/Configuration.cs
@@ -1,9 +1,6 @@
 namespace ConsoleApplication1.Migrations
 {
-    using System;
-    using System.Data.Entity;
     using System.Data.Entity.Migrations;
-    using System.Linq;
 
     internal sealed class Configuration : DbMigrationsConfiguration<TodoApp.Models.TodoContext>
     {
